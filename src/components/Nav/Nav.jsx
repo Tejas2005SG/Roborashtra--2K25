1d88e23f02284a679da807d9cf2b39666f6477a3
Shreeram <shreeram.shanmugasundaram_entc21@pccoer.in>
2024-01-08 23:40:22 +0530

    Update Nav - Font, hover, Logo remove[mob]

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,10 +3,12 @@ import React, { useState } from "react";
 import Image from "next/image";
 import logo from "@/images/logo.png";
 import "./Nav.css";
-import Bt from "../Buttons/Btn1";
 import Link from "next/link";
 import { GiHamburgerMenu } from "react-icons/gi";
-import Btn1 from "../Buttons/Btn1";
+
+import { Play } from "next/font/google";
+
+const play = Play({ subsets: ["latin"], weight: ["400"] });
 
 const options = [
   {
@@ -36,14 +38,17 @@ const Nav = () => {
   let menustyle = menu ? { left: "0%" } : { left: "-110%" };
   return (
     <div>
-      <nav style={{ padding: "1rem 0" }}>
-        <Image src={logo} width={275} height={100} />
+      <nav>
+        <Link href="/">
+          <Image src={logo} width={275} height={100} />
+        </Link>
 
         <ul className="desktop-links" style={menustyle}>
           {options.map((option) => {
             return (
               <li key={option.opt}>
                 <Link
+                  className={play.className}
                   href={option.link}
                   onClick={() => setMenu((prev) => !prev)}
                 >

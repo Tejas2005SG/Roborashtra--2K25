1619763a300e5fcc7159af941b72d4036d398619
Vishal Jangid <vishal.jangid_it22@pccoer.in>
2024-01-10 09:22:39 +0530

    Sponsors page updated

--- src/app/menu/sponsors/page.jsx
+++ src/app/menu/sponsors/page.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import Member from "@/components/Member/Member";
+import "./../teams/teams.css";
+import "../clan/clan.css";
+import member1 from "./img/google.png";
 
-const Sponsors = () => {
-  return <div className=" text-center text-lg">Coming Soon</div>;
-};
+const members = [
+  { img: member1,
+  img: member1, 
+  img: member1 }
+];
 
-export default Sponsors;
+
+  export default function Sponsors(){
+    return(
+    <div className=" text-center text-lg">Coming Soon</div>
+  //  <div className="team" style={{ }}>
+  //     <div className="team_wrapper">
+  //       <h2>Title Sponsors</h2>
+  //       <div className="team_row">
+  //         {members.map((member) => {
+  //           return (
+  //             <>
+  //               <Member name={member.name}/>
+  //             </>
+  //           );
+  //         })}
+  //         </div>
+
+
+  //       <h2>Associative Sponsor</h2>
+  //       <div className="team_row">
+  //         {members.map((member) => {
+  //           return (
+  //             <>
+  //               <Member name={member.name} />
+  //             </>
+  //           );
+  //         })}
+  //       </div> 
+  //     </div>
+  //   </div> 
+    );
+}
+

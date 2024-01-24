d68fa23555ca6367a6393273147958eb3bfa930f
Vishal Jangid <vishal.jangid_it22@pccoer.in>
2024-01-24 22:33:05 +0530

    Revert "Event cards and main page changes"
    This reverts commit 75775e31d0e84de4d97ca8a225b17c1b73e729cb.

--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import Image from 'next/image';
-
+import bottomArrow from '../../images/arrow.png';
 
 import Card from "../../components/Card/Card";
 import "./events.css";
@@ -33,27 +33,67 @@ const events = [
   },
 ];
 
+const events2 = [
+  {
+    title: "SubmersiGrip Rover",
+    desc: "Remotely Operated Hybrid",
+    link: "/pg_events/submersiGripRover",
+    image: "/pg_e1.svg",
+  },
+  {
+    title: "Autonomous Drive Race",
+    desc: "Manipulator-Enabled Mobile Robot",
+    link: "/pg_events/AutonomusDrive",
+    image: "/pg_e2.svg",
+  },
+  {
+    title: "Robo Butler Pioneer",
+    desc: "Manipulator-Enabled Mobile Robot",
+    link: "/pg_events/RoboButlerPioneer",
+    image: "/pg_e3.svg",
+  },
+  {
+    title: "AutoplaceROS Dynamo",
+    desc: "check the top-level render call using",
+    link: "/pg_events/AutoplaceRos",
+    image: "/pg_e4.svg",
+  },
+
+];
 
 const Cards = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const togglePage = () => {
+    setCurrentPage((prevPage) => (prevPage === 1 ? 2 : 1));
+  };
+
+  const currentEvents = currentPage === 1 ? events : events2;
+  const pageTitle = currentPage === 1 ? "Dvanadvayuddh" : "Ransangram";
 
   return (
     <>
-
+      <div className={`page${currentPage}`}>
         <div className="Container">
+          <h1 className="headTitle">{pageTitle}</h1>
           <div className="Events">
             {currentEvents.map((event) => {
               return (
                 <Card
-                  key={events.title}
-                  title={events.title}
-                  desc={events.desc}
-                  link={events.link}
-                  image={events.image}
+                  key={event.title}
+                  title={event.title}
+                  desc={event.desc}
+                  link={event.link}
+                  image={event.image}
                 />
               );
             })}
           </div>
+          <button className="bottomButton" onClick={togglePage} style={{transition:""}}>
+              <Image src={bottomArrow} alt="arrow" srcset="" />
+          </button>
         </div>
+      </div>
     </>
   );
 };

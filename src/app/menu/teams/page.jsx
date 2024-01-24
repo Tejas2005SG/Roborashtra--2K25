b1642e82c19bbfa9ab913e840f5b752bc063e37a
Vishal Jangid <vishal.jangid_it22@pccoer.in>
2024-01-24 21:43:40 +0530

    Committing local changes before revert

--- src/app/menu/teams/page.jsx
+++ src/app/menu/teams/page.jsx
@@ -1,173 +1,23 @@
-import coordinator from "./img/Mahendra Salunkhe .jpeg";
-import lead from "./img/Om Khare.jpg";
-import colead from "./img/Khushi Rohra.jpg";
-
-import des1 from "./img/Design/Vedant _Patil.jpg";
-import des2 from "./img/Design/Shreya_kale.png";
-import des3 from "./img/Design/Suyash_wagde.jpg";
-import des4 from "./img/Design/aastha.jpg";
-
-import web1 from "./img/Web/Abhishek Deshmukh.jpg";
-import web2 from "./img/Web/Aditya_Gaikwad.jpg";
-import web3 from "./img/Web/Vishal Jangid.png";
-
-import s1 from "./img/Steel_soldier_slam/Aditya Pardeshi.jpg";
-import s2 from "./img/Steel_soldier_slam/Arya Godase.jpg";
-import s3 from "./img/Steel_soldier_slam/Avdhoot.jpg";
-import s4 from "./img/Steel_soldier_slam/Kush Agarwal.jpg";
-import s5 from "./img/Steel_soldier_slam/Manas Ghormade.jpg";
-import s6 from "./img/Steel_soldier_slam/Sneha.jpg";
-import s7 from "./img/Steel_soldier_slam/Swanand.jpg";
-import s8 from "./img/Steel_soldier_slam/Tejasvi.jpg";
-
-import pr1 from "./img/Public_relation/Girish_Jagtap.jpeg";
-import pr2 from "./img/Public_relation/Harsh_Palande.jpeg";
-import pr3 from "./img/Public_relation/Manasi_Velhal.jpeg";
-import pr4 from "./img/Public_relation/Pranay_Dhanve.jpg";
-import pr5 from "./img/Public_relation/Prathmesh_Deshpande.jpeg";
-import pr6 from "./img/Public_relation/Sahil_Aher.jpeg";
-import pr7 from "./img/Public_relation/Samruddhi_Patil.jpg";
-import pr8 from "./img/Public_relation/Sandesh_Lanke.jpeg";
-import pr9 from "./img/Public_relation/Shreyash_Kunjir.jpeg";
-import pr10 from "./img/Public_relation/Shreyash_Pawar.jpeg";
-
-import m1 from "./img/Management/Abhinandan.jpeg";
-import m2 from "./img/Management/Aditi Kshirsagar _Management .jpg";
-import m3 from "./img/Management/Ashish Saini_Accommodation.png";
-import m4 from "./img/Management/Atharv Borhade_Accomodation.jpg";
-import m5 from "./img/Management/chetan_managment.jpg";
-import m6 from "./img/Management/Darshan Patgar_Management.jpg";
-import m7 from "./img/Management/Dhiraj_management.jpg";
-import m8 from "./img/Management/Kaustubh_Management.jpg";
-import m9 from "./img/Management/Madhav Surwase_Accommodation.jpg";
-import m10 from "./img/Management/Mayuri Yenare_Accessories.jpg";
-import m11 from "./img/Management/member1.jpg";
-import m12 from "./img/Management/member2.jpg";
-import m13 from "./img/Management/member3.jpg";
-import m14 from "./img/Management/member4.jpg";
-import m15 from "./img/Management/Nikita Singh management.jpg";
-import m16 from "./img/Management/Omkar_panchal_computer.jpg";
-import m17 from "./img/Management/Prachi Thorat_Accessories.jpg";
-import m18 from "./img/Management/Prathamesh walekar.jpg";
-import m19 from "./img/Management/pratikchavare.jpg";
-import m20 from "./img/Management/Sanskar shinde.jpg";
-import m21 from "./img/Management/Sayali Metkar.png";
-import m22 from "./img/Management/Shraddha Dagdu.jpg";
-import m23 from "./img/Management/Shravani Ladkat.jpg";
-import m24 from "./img/Management/Siddhesh Rohokale.jpg";
-import m25 from "./img/Management/Vedant Mahadi.jpg";
-import m26 from "./img/Management/Vedant Raut.png";
-
-
-
-
+import member1 from "./img/a.jpg";
 import Member from "@/components/Member/Member";
 import "./teams.css";
 import "../clan/clan.css";
-
-const man = [
-  { img: m1, name: "Abhinandan" },
-  { img: m2, name: "Aditi Kshirsagar" },
-  { img: m3, name: "Ashish Saini" },
-  { img: m4, name: "Atharva Borhade" },
-  { img: m5, name: "Chetan Phulmante" },
-  { img: m6, name: "Darshan Patgar" },
-  { img: m7, name: "Dhiraj Marathe" },
-  { img: m8, name: "Kaustubh" },
-  { img: m9, name: "Madhav Survase" },
-  { img: m10, name: "Mayuri Yenare" },
-  { img: m11, name: "Member 1" },
-  { img: m12, name: "Member 2" },
-  { img: m13, name: "Member 3" },
-  { img: m14, name: "Member 4" },
-  { img: m15, name: "Nikita Singh" },
-  { img: m16, name: "Omkar Panchal" },
-  { img: m17, name: "Prachi Thorat" },
-  { img: m18, name: "Prathamesh Walekar" },
-  { img: m19, name: "Pratik Chaware" },
-  { img: m20, name: "Sanskar Shinde" },
-  { img: m21, name: "Sayali Metkar" },
-  { img: m22, name: "Shraddha Dagdu" },
-  { img: m23, name: "Shravani Ladkat" },
-  { img: m24, name: "Siddhesh Rohokale" },
-  { img: m25, name: "Vedant Mahadi" },
-  { img: m26, name: "Vedant Raut" },
+const members = [
+  { img: member1, name: "Shreeram" },
+  { img: member1, name: "Shreeram" },
+  { img: member1, name: "Shreeram" },
+  { img: member1, name: "Shreeram" },
 ];
-
-const pr = [
-  { img: pr1, name: "Aditya Pardeshi" },
-  { img: pr2, name: "Arya Godase" },
-  { img: pr3, name: "Avdhoot" },
-  { img: pr4, name: "Kush Agarwal" },
-  { img: pr5, name: "Manas Ghormade" },
-  { img: pr6, name: "Sneha" },
-  { img: pr7, name: "Swanand" },
-  { img: pr8, name: "Tejasvi" },
-  { img: pr9, name: "Sneha" },
-  { img: pr10, name: "Swanand" },
-];
-
-const sss = [
-  { img: s1, name: "Aditya Pardeshi" },
-  { img: s2, name: "Arya Godase" },
-  { img: s3, name: "Avdhoot" },
-  { img: s4, name: "Kush Agarwal" },
-  { img: s5, name: "Manas Ghormade" },
-  { img: s6, name: "Sneha" },
-  { img: s7, name: "Swanand" },
-  { img: s8, name: "Tejasvi" },
-];
-
-const des = [
-  { img: des1, name: "Vedant Patil" },
-  { img: des2, name: "Shreya Kale" },
-  { img: des3, name: "Suyash Wagde" },
-  { img: des4, name: "Aastha" },
-];
-
-const web = [
-  { img: web1, name: "Abhishek Deshmukh" },
-  { img: web2, name: "Aditya Gaikwad" },
-  { img: web3, name: "Vishal Jangid" },
-];
-
-
 export default function Teams() {
   return (
     <div className=" text-center text-lg">
-      <div className="team" >
+      Team Coming soon
+      {/* <div className="team" style={{ display: "none" }}>
+        <h1>Our Team</h1>
         <div className="team_wrapper">
-
-
-          <h2 className="text-center text-lg">Event Co-ordinator</h2>
+          <h2>Core Team</h2>
           <div className="team_row">
-                <Member name={"Mahendra Salunkhe"} image={coordinator} />
-          </div>
-
-          <h2 className="text-center text-lg">Lead</h2>
-          <div className="team_row">
-                <Member name={"Om Khare"} image={lead} />
-          </div>
-
-          <h2 className="text-center text-lg">Co-Lead</h2>
-          <div className="team_row">
-                <Member name={"Khushi rohra"} image={colead} />
-          </div>
-
-          <h2 className="text-center text-lg">Web Team</h2>
-          <div className="team_row">
-            {web.map((member) => {
-              return (
-                <div className="member_box">
-                  <Member name={member.name} image={member.img} />
-                </div>
-              );
-            })}
-          </div>
-          
-          <h2 className=" text-center text-lg">Design Team</h2>
-          <div className="team_row">
-            {des.map((member) => {
+            {members.map((member) => {
               return (
                 <>
                   <Member name={member.name} image={member.img} />
@@ -176,9 +26,9 @@ export default function Teams() {
             })}
           </div>
 
-          <h2>Steel Soldier Slam Team</h2>
+          <h2>Complete Team</h2>
           <div className="team_row">
-            {sss.map((member) => {
+            {members.map((member) => {
               return (
                 <>
                   <Member name={member.name} image={member.img} />
@@ -186,31 +36,8 @@ export default function Teams() {
               );
             })}
           </div>
-
-          <h2 className=" text-center text-lg">Public Reaction Team</h2>
-          <div className="team_row">
-            {pr.map((member) => {
-              return (
-                <div className="member_box">
-                  <Member  name={member.name} image={member.img} />
-                </div>
-              );
-            })}
-          </div>
-
-          <h2 className=" text-center text-lg">Management Team</h2>
-          <div className="team_row">
-            {man.map((member) => {
-              return (
-                <>
-                  <Member name={member.name} image={member.img} />
-                </>
-              );
-            })}
-          </div>
-
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }

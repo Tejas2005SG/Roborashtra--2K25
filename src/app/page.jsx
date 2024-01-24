75775e31d0e84de4d97ca8a225b17c1b73e729cb
Vishal Jangid <vishal.jangid_it22@pccoer.in>
2024-01-24 22:25:17 +0530

    Event cards and main page changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,8 @@
 import Nav from "@/components/Nav/Nav";
 import Image from "next/image";
 import MainCharater from "../images/mainChar.png";
-import drdo from "../images/DRDO.png";
-import diat from "../images/DIAT.jpg";
-import handshake from "../images/Handshake.png";
-import pcet from "../images/PCET.jpg";
-import pccoer from "../images/PCCOER.jpg";
-
 import "./main.css";
-import Btn1 from "@/components/Buttons/Btn1";
+import Btn4 from "@/components/Buttons/Btn4";
 export default function Home() {
   return (
     <div className="wrapper">
@@ -26,22 +20,12 @@ export default function Home() {
           <span>Rashtra</span>
         </div>
         <div className="main_bottom">
-          <p>“The Rise Of RoboNation Bharat Leads The Way”</p>
+          <p>“Rise Of RoboNation Bharat Leads The Way”</p>
           <div className="start_btn">
-            <Btn1 text={"Start"} link={"/menu"} />
+            <Btn4 link={"/menu"} />
           </div>
         </div>
       </main>
-
-      <div className="bottomLogo">
-        <Image src={diat} alt="diat" srcset="" />
-        <Image src={drdo} alt="drdo" srcset="" />
-        {/* <Image src={handshake} alt="handshake" srcset="" /> */}
-        <h1>X</h1>
-        <Image src={pcet} alt="pcet" srcset="" />
-        <Image src={pccoer} alt="pccoer" srcset="" />
-
-      </div>
     </div>
   );
 }

da29880f96f0feff5733c678087f964ce9da21ee
Vishal Jangid <vishal.jangid_it22@pccoer.in>
2024-01-28 21:47:51 +0530

    Team Page Change

--- src/app/menu/teams/page.jsx
+++ src/app/menu/teams/page.jsx
@@ -82,84 +82,87 @@ export default function Teams() {
 
   return (
     
-    <div className=" text-center text-lg">
-      Coming Soon </div>
-      /* <div className="team" >
-        <div className="team_wrapper">
+    // <div className=" text-center text-lg">
+    //   Coming Soon </div>
+    <div className="team" >
+    <div className="team_wrapper">
 
 
-          <h2 className="text-center text-lg">Event Co-ordinator</h2>
-          <div className="team_row">
-                <Member name={"Mahendra Salunkhe"} image={coordinator} />
-          </div>
+      <h2 className="text-center text-lg">Event Co-ordinator</h2>
+      <div className="team_row">
+            <Member name={"Dr.Mahendra Salunkhe"} image={coordinator} position={"Event Co-ordinator"}/>
+      </div>
 
-          <h2 className="text-center text-lg">Lead</h2>
-          <div className="team_row">
-                <Member name={"Om Khare"} image={lead} />
-          </div>
+      <h2 className="text-center text-lg">Lead</h2>
+      <div className="team_row">
+            <Member name={"Om Khare"} image={lead} position={"Team Lead"}/>
+      </div>
 
-          <h2 className="text-center text-lg">Co-Lead</h2>
-          <div className="team_row">
-                <Member name={"Khushi rohra"} image={colead} />
-          </div>
-
-          <h2 className="text-center text-lg">Web Team</h2>
-          <div className="team_row">
-            {web.map((member) => {
-              return (
-                <>
-                  <Member name={member.name} image={member.img} />
-                </>
-              );
-            })}
-          </div>
-          
-          <h2 className=" text-center text-lg">Design Team</h2>
-          <div className="team_row">
-            {des.map((member) => {
-              return (
-                <>
-                  <Member name={member.name} image={member.img} />
-                </>
-              );
-            })}
-          </div>
-
-          <h2>Steel Soldier Slam Team</h2>
-          <div className="team_row">
-            {sss.map((member) => {
-              return (
-                <>
-                  <Member name={member.name} image={member.img} />
-                </>
-              );
-            })}
-          </div>
-
-          <h2 className=" text-center text-lg">Public Reaction Team</h2>
-          <div className="team_row">
-            {pr.map((member) => {
-              return (
-                <>
-                  <Member 
-                   name={member.name} image={member.img} />
-                </>
-              );
-            })}
-          </div>
-
-          <h2 className=" text-center text-lg">Management Team</h2>
-          <div className="team_row">
-            {man.map((member) => {
-              return (
-                <>
-                  <Member name={member.name} image={member.img} />
-                </>
-              );
-            })}
-          </div>
+      <h2 className="text-center text-lg">Co-Lead</h2>
+      <div className="team_row">
+      <Member name={"Chandrakant Raut"} image={colead2} position={"Management Lead"}/>
+            <Member name={"Khushi rohra"} image={colead} position={"Finance Lead"}/>
+      </div>
+      
+      <h2 className=" text-center text-lg">Management Team</h2>
+      <div className="team_row">
+        {man.map((member) => {
+          return (
+            <>
+              <Member name={member.name} image={member.img} />
+            </>
+          );
+        })}
+      </div>
+      
+      <h2 className=" text-center text-lg">Public Reaction Team</h2>
+      <div className="team_row">
+        {pr.map((member) => {
+          return (
+            <>
+              <Member 
+              name={member.name} image={member.img} />
+            </>
+          );
+        })}
+      </div>
+      
+      <h2 className=" text-center text-lg">Design Team</h2>
+      <div className="team_row">
+        {des.map((member) => {
+          return (
+            <>
+              <Member name={member.name} image={member.img} position={member.position} />
+            </>
+          );
+        })}
         </div>
-      </div> */
+        
+      <h2 className="text-center text-lg">Web Team</h2>
+      <div className="team_row">
+        {web.map((member) => {
+          return (
+            <>
+              <Member name={member.name} image={member.img} position={member.position}/>
+            </>
+          );
+        })}
+      </div>
+
+      <h2>Steel Soldier Slam Team</h2>
+      <div className="team_row">
+        {sss.map((member) => {
+          return (
+            <>
+              <Member name={member.name} image={member.img} position={member.position}/>
+            </>
+          );
+        })}
+      </div>
+
+
+    </div>
+  </div>
 
   );
 }

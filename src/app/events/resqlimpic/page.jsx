8f657e8a4d951a5adba9ddf4459c7d0ab9c02d65
Vishal Jangid <vishal.jangid_it22@pccoer.in>
2024-02-19 00:02:06 +0530

    Resqlympic info update

--- src/app/events/resqlimpic/page.jsx
+++ src/app/events/resqlimpic/page.jsx
@@ -11,7 +11,7 @@ const CamWarrior = () => {
       <div className="Event-content resq">
       <h1 className="EventTitle">ResQlympic</h1>
         <p>
-        Welcome to Resqlympic, where robots demonstrate life-saving rescue operations! This thrilling event showcases the incredible abilities of robots navigating challenging scenarios to extract people in distress. Join us for a celebration of innovation, compassion, and the vital role of technology in humanitarian efforts. Get ready for a remarkable fusion of cutting-edge robotics and the noble mission of saving lives in the most demanding situations.
+        It is your chance to showcase your skills. We bring you a competition that will test your navigation skills along with your robot-building capabilities. You have to build a robot such that it can grab the victims and relocate them to a safe location without causing any harm to any of the victims. You will have to navigate the arena to find the victims. But, here is the catch, among other obstacles, there will be mines present. The mine goes off in 2 minutes, and you have two minutes to rescue the hostages, if u need more time, find the switches and disable the mines. You get a maximum of 10 minutes, the choice of the hostages and mines is up to you. Your decision-making skill is your ultimate tool.
         </p>
         <div className="action_btn">
           <Btn2 link={"/resqlympics.pdf"} />

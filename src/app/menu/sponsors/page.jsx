0123f54cbc4a3368805daebf05efa81b0172dfa2
Vishal Jangid <vishal.jangid_it22@pccoer.in>
2024-03-26 20:17:44 +0530

    Sponsors Update

--- src/app/menu/sponsors/page.jsx
+++ src/app/menu/sponsors/page.jsx
@@ -5,6 +5,9 @@ import unstop from "../../../images/Unstop.jpg";
 import drdo from "../../../images/Drdo.png"
 import imperial from "../../../images/imperial.png"
 import cogna from "../../../images/cogna.png"
+import insidefpv from "../../../images/insidefpv.png"
+import powerPlanner from "../../../images/powerPlaneer.jpg"
+import mitu from "../../../images/mitu.png"
 
 
   export default function Sponsors(){
@@ -18,10 +21,17 @@ import cogna from "../../../images/cogna.png"
          <h3>Powered By</h3>
          <Image src={unstop} alt="" srcset="" />
 
+         <h3>Drone Partner</h3>
+        <div className="dronePartner">
+         <Image src={insidefpv} alt="" srcset=""  />
+         </div>
+
           <h3>Associative Sponsor</h3>
          <div className="sponsor_row">
           <Image src={imperial} alt="" srcset="" />
           <Image src={cogna} alt="" srcset="" />
+          <Image src={powerPlanner} alt="" srcset="" />
+          <Image src={mitu} alt="" srcset="" />
 
          </div> 
        </div>

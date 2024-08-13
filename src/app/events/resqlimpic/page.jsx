f91cfe486e8c1ebd8be4b9b3f9b26b02ec5af2ee
Trushant Ramdas Jadhav <trushantramdasjadhav@Trushants-MacBook-Air.local>
2024-08-13 20:11:59 +0530

    Stagged changes

--- src/app/events/resqlimpic/page.jsx
+++ src/app/events/resqlimpic/page.jsx
@@ -10,7 +10,7 @@ const CamWarrior = () => {
     <div className="Event">
       <div className="Event-content resq">
       <h1 className="EventTitle">ResQlympic</h1>
-        <p>
+        <p className="Content">
         It is your chance to showcase your skills. We bring you a competition that will test your navigation skills along with your robot-building capabilities. You have to build a robot such that it can grab the victims and relocate them to a safe location without causing any harm to any of the victims. You will have to navigate the arena to find the victims. But, here is the catch, among other obstacles, there will be mines present. The mine goes off in 2 minutes, and you have two minutes to rescue the hostages, if u need more time, find the switches and disable the mines. You get a maximum of 10 minutes, the choice of the hostages and mines is up to you. Your decision-making skill is your ultimate tool.
         <h2 className="headContact">For any event related query <br />Contact Mehul(Event Head) : 9307705698</h2>
         <div className="wpYoutube">

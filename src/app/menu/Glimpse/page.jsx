e74c50f5fbc40b30e8bfc256bfd3b260c46399bf
Trushant Ramdas Jadhav <trushantramdasjadhav@Trushants-MacBook-Air.local>
2024-08-23 21:47:04 +0530

    Glimpse and Tagline

--- src/app/menu/Glimpse/page.jsx
+++ src/app/menu/Glimpse/page.jsx
@@ -10,13 +10,17 @@ import glimpse2 from "@/images/card2.jpg"
 function gallery() {
   return (
     <div className='gallery_wrapper'>
-        <h1>Our Gallery</h1>
+        <br></br>
+        <h1 >Our Gallery</h1>
         <div className="imgs">
           <GlimpseCard imgUrl={"https://drive.google.com/uc?export=download&id=1FkKK0sT_VUvpNw1HZbWQdm9ycjJtBslZ"}/>
           <GlimpseCard imgUrl={"https://drive.google.com/uc?export=download&id=1wurWBu9dzXSoEUf7N-uLuuPFwmOETEBw"}/>
           <GlimpseCard  imgUrl={"https://drive.google.com/uc?export=download&id=15a07v3oXjdQQiw5mIHbceXtbGemym0dF"}/>
           <GlimpseCard imgUrl={"https://drive.google.com/uc?export=download&id=1LVo70QREC8lBUrWxCQAHlPNSbPKlMSms"}/>
-          <GlimpseCard  imgUrl={glimpse1}/>
+          <GlimpseCard imgUrl={"https://drive.google.com/uc?export=download&id=1LVo70QREC8lBUrWxCQAHlPNSbPKlMSms"}/>
+         
+          <GlimpseCard imgUrl={"https://drive.google.com/uc?export=download&id=1RAuXh3mjnOKY-CeD6w_qvDdy7uKtwmoZ"}/>
+         
           <GlimpseCard imgUrl={glimpse2}/>
 
     

deffca3653b2e0538231636b79acca55bc3db540
Vishal Jangid <vishal.jangid_it22@pccoer.in>
2024-09-17 17:06:02 +0530

    Responsive update2

--- src/app/events/layout.jsx
+++ src/app/events/layout.jsx
@@ -8,7 +8,7 @@ import Cursor from "@/components/Cursor/cursor";
 export const metadata = {
   title: "Roborashtra",
   description:
-    "Experience the thrill of Roborashtra, an inter-college robotics extravaganza organized by Pimpri Chinchwad College of Engineering and Research (PCCOER). Compete in exciting events like Robowar and Cam Warrior for a chance to win prizes worth 2 lakh rupees. Don't miss the opportunity to showcase your innovation and engineering skills. ",
+    "Experience the thrill of Roborashtra, an inter-college robotics extravaganza organized by Pimpri Chinchwad College of Engineering and Research (PCCOER). Compete in exciting events like YantraUtsav and Reqlympic for a chance to win prizes worth 2 lakh rupees. Don't miss the opportunity to showcase your innovation and engineering skills. ",
 };
 
 export default function EventLayout({ children }) {
--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import "./globals.css";
 export const metadata = {
   title: "Roborashtra",
   description:
-    "Experience the thrill of Roborashtra, an inter-college robotics extravaganza organized by Pimpri Chinchwad College of Engineering and Research (PCCOER). Compete in exciting events like Robowar and Cam Warrior for a chance to win prizes worth 2 lakh rupees. Don't miss the opportunity to showcase your innovation and engineering skills. ",
+    "Experience the thrill of Roborashtra, an inter-college robotics extravaganza organized by Pimpri Chinchwad College of Engineering and Research (PCCOER). Compete in exciting events like YantraUtsav and Reqlympic for a chance to win prizes worth 2 lakh rupees. Don't miss the opportunity to showcase your innovation and engineering skills. ",
 };
 
 export default function RootLayout({ children }) {
